beca1f8126d032e07fa3d0f85eb33780f8ad6498
romanima <118591502+romanima@users.noreply.github.com>
2026-04-19 19:50:20 +0700

    Update database.py

--- database.py
+++ database.py
@@ -32,7 +32,7 @@ async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-async def reset_db():
+async def reset_db(engine: AsyncEngine):
     """
     Очистка всех таблиц в БД (для тестов).
     """
