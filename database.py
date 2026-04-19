1026ca7148312194093cf18beef9a0282490f061
romanima <118591502+romanima@users.noreply.github.com>
2026-04-19 15:30:02 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,29 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker, create_async_engine
-from models import async_session
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
 DB_URL = "sqlite+aiosqlite:///./db.sqlite3"
 
 Base = declarative_base()
-engine: AsyncEngine = create_async_engine(DB_URL, echo=True, future=True)
-AsyncSessionLocal = sessionmaker(
+
+engine: AsyncEngine = create_async_engine(
+    DB_URL,
+    echo=True,
+    future=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
-    autoflush=False  # явно указываем autoflush, чтобы избежать значения по умолчанию
-)  # type: ignore
+    autoflush=False
+)
 
 async def get_async_session():
     """
     Провайдер асинхронной сессии.
     """
-    async with async_session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
-
 
 async def init_db():
     """
@@ -28,10 +32,9 @@ async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-
 async def reset_db():
     """
-    Очистка всех таблиц в БД. (для тестов в мое случае)
+    Очистка всех таблиц в БД (для тестов).
     """
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
